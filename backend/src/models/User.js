d8a269415d4151dc07b753317d632f31d21a07b2
Jedev1 <almeidadesousajoaoenrique@gmail.com>
2025-10-10 09:31:05 -0300

    💾 Feat(API): Add Login Route

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -7,32 +7,36 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
-    email:{
+    email: {
         type: String,
         required: true,
         unique: true,
     },
-    password:{
+    password: {
         type: String,
         required: true,
-        minlength: 6
+        minlength: 6,
     },
-    profileImage:{
+    profileImage: {
         type: String,
-        default: ""
-    }
-})
+        default: "",
+    },
+});
 
-// hash password before saving user to db
-userSchema.pre("save", async function(next){
-    if(!this.isModified("password")) return next();
+// Hash password before saving user to DB
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
 
     const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt)
+    this.password = await bcrypt.hash(this.password, salt);
 
     next();
-})
+});
 
-const User = mongoose.model("User", userSchema)
+// Compare password function
+userSchema.methods.comparePassword = async function (userPassword) {
+    return await bcrypt.compare(userPassword, this.password);
+};
 
+const User = mongoose.model("User", userSchema);
 export default User;
