cd541f1ccb55c1397d01459ecefcb07671ecb765
Jedev1 <almeidadesousajoaoenrique@gmail.com>
2025-10-10 10:54:44 -0300

    💾 Feat(API): Create GET Books with paginations

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -34,3 +34,30 @@ router.post("/", protectRoute, async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+// const response = await fetch("http://localhost:3000/api/books?page=1&limit=5");
+
+//pagination => infinite loading
+router.get("/", protectRoute, async (req, res) => {
+    try {
+        const page = req.query.page || 1;
+        const limit = req.query.limit || 5;
+        const skip = (page - 1) * limit;
+
+        const books = await Book.find().sort({ createdAt: -1 }).skip(skip).limit(limit).populate("User", "username profileImage")
+
+        const totalBooks = await Book.countDocuments();
+
+        res.send({
+            books,
+            currentPage: page,
+            totalBooks,
+            totalPages: Math.ceil( totalBooks / limit )
+        })
+    } catch (error) {
+        console.log("Error in get all books route", error);
+        res.status(500).json({message: "Internal server error"})
+    }
+})
+
+export default router;
