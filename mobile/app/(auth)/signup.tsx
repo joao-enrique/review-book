868f36cd420cee527a301094a7634ce9c35098d8
joao-enrique <almeidadesousajoaoenrique@gmail.com>
2025-10-10 16:11:15 -0300

    💽 Chore(Mobile): implementing fetch sigup screen

--- mobile/app/(auth)/signup.tsx
+++ mobile/app/(auth)/signup.tsx
@@ -1,20 +1,26 @@
-import { View, Text, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { View, Text, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, ActivityIndicator, Alert } from 'react-native'
 import React, { useState } from 'react'
 import styles from '@/assets/styles/login.styles'
 import { Ionicons } from '@expo/vector-icons'
 import COLORS from '@/constants/colors'
-import { router, useRouter } from 'expo-router'
+import { useRouter } from 'expo-router'
+import { useAuthStore } from "../../store/authStore.js"
 
 export default function Signup() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const {user, isLoading, register, token } = useAuthStore();
 
   const router = useRouter();
 
-  const handleSignUp = () => {}
+  const handleSignUp = async () => {
+    const result = await register(username, email, password);
+
+    if(!result.success) Alert.alert("Error:", result.error);
+    )
+  }
 
   return (
     <KeyboardAvoidingView style={{flex: 1}} behavior={Platform.OS === "ios" ? "padding" : "height"}>
