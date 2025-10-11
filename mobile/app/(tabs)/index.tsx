5313b4a7f9f831e4778c2cd347856725c6fafda4
Jedev1 <almeidadesousajoaoenrique@gmail.com>
2025-10-11 16:19:39 -0300

    💾 Feat: Add Home Screen

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,11 +1,18 @@
-import { View, Text, TouchableOpacity, Image, FlatList } from 'react-native'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator, RefreshControl } from 'react-native'
 import { useEffect, useState } from 'react'
 import { useAuthStore } from '@/store/authStore'
 import styles from "@/assets/styles/home.styles.js"
 import { API_URL } from '@/constants/api';
+import { Image } from "expo-image"
+import COLORS from '@/constants/colors';
+import { Ionicons } from '@expo/vector-icons';
+import { formatPublishDate } from "../../lib/utils";
+import Loader from '@/components/Loader';
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 export default function Home() {
-  const { token } = useAuthStore();
+  const { token, logout } = useAuthStore();
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
@@ -24,15 +31,27 @@ export default function Home() {
       const data = await response.json();
       if(!response.ok) throw new Error(data.message || "Failed to fetch books")
 
-      setBooks((prevBooks) => [...prevBooks, ...data.books])
+      // todo fix it lates
+      // setBooks((prevBooks) => [...prevBooks, ...data.books])
+
+       const uniqueBooks =
+        refresh || pageNum === 1
+          ? data.books
+          : Array.from(new Set([...books, ...data.books].map((book) => book._id))).map((id) =>
+              [...books, ...data.books].find((book) => book._id === id)
+            );
+
+      setBooks(uniqueBooks);
 
       setHasMore(pageNum < data.totalPages)
       setPage(pageNum)
     } catch (error) {
       console.log("Error fetching books", error)
     } finally {
-      if(refresh) setRefreshing(false);
-      else setLoading(false)
+      if(refresh){
+        await sleep(800);
+        setRefreshing(false);
+      }else setLoading(false)
     }
   }
 
@@ -40,7 +59,11 @@ export default function Home() {
     fecthBooks();
   }, [])
 
-  const handleLoadMore = async () => {}
+  const handleLoadMore = async () => {
+     if (hasMore && !loading && !refreshing) {
+      await fecthBooks(page + 1);
+    }
+  }
 
   const renderItem = ({item}) => (
     <View style={styles.bookCard}>
@@ -55,8 +78,32 @@ export default function Home() {
         <Image source={item.image} style={styles.bookImage} contentFit="cover" />
       </View>
 
+      <View style={styles.bookDetails}>
+        <Text style={styles.bookTitle}>{item.title}</Text>
+        <View style={styles.ratingContainer}>{renderRatingStars(item.rating)}</View>
+        <Text style={styles.caption}>{item.caption}</Text>
+        <Text style={styles.date}>Shared on {formatPublishDate(item.createdAt)}</Text>
+      </View>
     </View>
   )
+
+  const renderRatingStars = (rating) => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <Ionicons
+          key={i}
+          name={i <= rating ? "star" : "star-outline"}
+          size={16}
+          color={i <= rating ? "#f4b400" : COLORS.textSecondary}
+          style={{ marginRight: 2 }}
+        />
+      );
+    }
+    return stars;
+  };
+
+  if (loading) return <Loader />;
 
   return (
     <View style={styles.container}>
@@ -66,7 +113,37 @@ export default function Home() {
         keyExtractor={(item) => item._id}
         contentContainerStyle={styles.listContainer}
         showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={() => fecthBooks(1, true)}
+            colors={[COLORS.primary]}
+            tintColor={COLORS.primary}
+          />
+        }
+        onEndReached={handleLoadMore}
+        ListHeaderComponent={
+          <View style={styles.header}>
+            <Text style={styles.headerTitle}>Book Review 📖</Text>
+            <Text style={styles.headerSubtitle}>Discover great reads from the community👇</Text>
+          </View>
+        }
+        ListFooterComponent={
+          hasMore && books.length > 0 ? (
+            <ActivityIndicator style={styles.footerLoader} size="small" color={COLORS.primary} />
+          ) : null
+        }
+        ListEmptyComponent={
+          <View style={styles.emptyContainer}>
+            <Ionicons name="book-outline" size={60} color={COLORS.textSecondary} />
+            <Text style={styles.emptyText}>No recommendations yet</Text>
+            <Text style={styles.emptySubtext}>Be the first to share a book!</Text>
+          </View>
+        }
       />
+      <TouchableOpacity onPress={logout}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </View>
   )
 }
